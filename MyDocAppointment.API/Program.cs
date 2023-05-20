73bdca49e4315fcc86f0a6879c14835fc335c539
Radu Chelaru <raduchelaru1@gmail.com>
2023-05-20 14:41:29 +0300

    cors added

--- MyDocAppointment.API/Program.cs
+++ MyDocAppointment.API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:4200")
+                          policy.WithOrigins("http://localhost:4200", "https://mydocappointmentfe.web.app")
                                                  .AllowAnyHeader()
                                                  .AllowAnyMethod();
                       });
