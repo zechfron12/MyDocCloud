1ea2e3e01796cd5ff43d17103e6a9e7389521a08
Radu Chelaru <raduchelaru1@gmail.com>
2023-05-27 16:32:55 +0300

    link added

--- MyDocAppointment.API/Program.cs
+++ MyDocAppointment.API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:4200", "https://mydocappointmentfe.web.app")
+                          policy.WithOrigins("http://localhost:4200", "https://mydocappointmentfe.web.app", "https://mydocappointmentfe.firebaseapp.com/")
                                                  .AllowAnyHeader()
                                                  .AllowAnyMethod();
                       });
